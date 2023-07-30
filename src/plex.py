9ec38cfce1d895e95b21ca724074257f28303cf2
Everett Southwick <everett@southwick.dev>
2023-07-30 14:32:11 -0400

    split parse into own function

--- src/plex.py
+++ src/plex.py
@@ -32,34 +32,42 @@ class PlexClient:
         sections = self.get_sections(section_type)
         for section in sections:
             for movie in section.all():
-                tmdb_id = next(
-                    (
-                        guid.id.split("tmdb://")[1].split("?")[0]
-                        for guid in movie.guids
-                        if guid.id.startswith("tmdb://")
-                    ),
-                    None,
-                )
-                imdb_db = next(
-                    (
-                        guid.id.split("imdb://")[1].split("?")[0]
-                        for guid in movie.guids
-                        if guid.id.startswith("imdb://")
-                    ),
-                    None,
-                )
-                path = movie.media[0].parts[0].file
-                if path:
-                    path = os.path.dirname(path)
-                min_date = datetime.now() - timedelta(days=self.config.last_watched_days_deletion_threshold)
-                history = self.plex._server.history(mindate=min_date, ratingKey=movie.ratingKey)
-                last_watched_date = max(entry.viewedAt for entry in history) if history else None
-
-                # Create Movie object and append to list
-                movie_obj = PlexMovie(tmdb_id, imdb_db, path, movie.title, movie.addedAt, last_watched_date)
+                movie_obj = self.parse_movie(movie)
                 movie_list.append(movie_obj)
 
         return movie_list
+
+    def parse_movie(self, movie):
+        """
+        Parses a Plex movie object into a PlexMovie object.
+
+        Args:
+            movie (plexapi.video.Movie): The Plex movie object to parse.
+
+        Returns:
+            PlexMovie: A PlexMovie object representing the movie.
+        """
+        tmdb_id = next(
+            (guid.id.split("tmdb://")[1].split("?")[0] for guid in movie.guids if guid.id.startswith("tmdb://")),
+            None,
+        )
+        imdb_db = next(
+            (guid.id.split("imdb://")[1].split("?")[0] for guid in movie.guids if guid.id.startswith("imdb://")),
+            None,
+        )
+        path = movie.media[0].parts[0].file
+        if path:
+            path = os.path.dirname(path)
+        title = movie.title
+        # Set it to 1970-01-01 if the movie was added before Plex started tracking that data
+        added_at = movie.addedAt if movie.addedAt else datetime.fromtimestamp(0)
+        min_date = datetime.now() - timedelta(days=self.config.last_watched_days_deletion_threshold)
+        history = self.plex._server.history(mindate=min_date, ratingKey=movie.ratingKey)
+        last_watched_date = max(entry.viewedAt for entry in history) if history else None
+
+        movie_obj = PlexMovie(tmdb_id, imdb_db, path, title, added_at, last_watched_date)
+
+        return movie_obj
 
     def get_series(self, section_type="show"):
         """
@@ -76,38 +84,46 @@ class PlexClient:
         sections = self.get_sections(section_type)
         for section in sections:
             for series in section.all():
-                tvdb_id = next(
-                    (
-                        guid.id.split("tvdb://")[1].split("?")[0]
-                        for guid in series.guids
-                        if guid.id.startswith("tvdb://")
-                    ),
-                    None,
-                )
-                imdb_db = next(
-                    (
-                        guid.id.split("imdb://")[1].split("?")[0]
-                        for guid in series.guids
-                        if guid.id.startswith("imdb://")
-                    ),
-                    None,
-                )
-                path = series.episodes()[0].media[0].parts[0].file
-
-                if path:
-                    # Get the season directory
-                    path = os.path.dirname(path)
-                    # Get the series directory
-                    path = os.path.dirname(path)
-                added_at = max(episode.addedAt for episode in series.episodes())
-                min_date = datetime.now() - timedelta(days=self.config.last_watched_days_deletion_threshold)
-                history = self.plex._server.history(mindate=min_date, ratingKey=series.ratingKey)
-                last_watched_date = max(entry.viewedAt for entry in history) if history else None
-
-                series_obj = PlexSeries(tvdb_id, imdb_db, path, series.title, added_at, last_watched_date)
+                series_obj = self.parse_series(series)
                 series_list.append(series_obj)
 
         return series_list
+
+    def parse_series(self, series):
+        """
+        Parses a Plex series object into a PlexSeries object.
+
+        Args:
+            series (plexapi.video.Show): The Plex series object to parse.
+
+        Returns:
+            PlexSeries: A PlexSeries object representing the series.
+        """
+        tvdb_id = next(
+            (guid.id.split("tvdb://")[1].split("?")[0] for guid in series.guids if guid.id.startswith("tvdb://")),
+            None,
+        )
+        imdb_db = next(
+            (guid.id.split("imdb://")[1].split("?")[0] for guid in series.guids if guid.id.startswith("imdb://")),
+            None,
+        )
+        path = series.episodes()[0].media[0].parts[0].file
+
+        if path:
+            # Get the season directory
+            path = os.path.dirname(path)
+            # Get the series directory
+            path = os.path.dirname(path)
+        title = series.title
+        added_at = series.addedAt if series.addedAt else datetime.fromtimestamp(0)
+        added_at = max(episode.addedAt for episode in series.episodes()) if series.episodes() else added_at
+        min_date = datetime.now() - timedelta(days=self.config.last_watched_days_deletion_threshold)
+        history = self.plex._server.history(mindate=min_date, ratingKey=series.ratingKey)
+        last_watched_date = max(entry.viewedAt for entry in history) if history else None
+
+        series_obj = PlexSeries(tvdb_id, imdb_db, path, title, added_at, last_watched_date)
+
+        return series_obj
 
     def get_sections(self, section_type):
         """
